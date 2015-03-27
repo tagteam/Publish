7e9c233b60748d483ebc330453aabe6c7fd5c1fd
Thomas Alexander Gerds <tag@biostat.ku.dk>
2015-03-27 14:24:05 +0100

    added new function pubformat

--- R/publish.anova.R
+++ R/publish.anova.R
@@ -1,15 +1,21 @@
-##' @export
-publish.anova <- function(object,digits=2,pvalue.digits=4,eps=0.0001,print=TRUE,residuals=FALSE,...){
-  x <- object
-  if (residuals==FALSE)
-    x=x[-NROW(x),]
-  names(x) <- c("Degrees of freedom","Sum of squares","Mean of squares","F value","p value")
-  x$"Sum of squares"=format(x$"Sum of squares",digits=digits,nsmall=digits)
-  x$"Mean of squares"=format(x$"Mean of squares",digits=digits,nsmall=digits)
-  x$"F value"=format(x$"F value",digits=digits,nsmall=digits)
-  x$"p value"=sapply(x$"p value",format.pval,digits=pvalue.digits,eps=eps)
-  if (print==TRUE){
-    out <- suppressMessages(publish.matrix(x,col1name="Factor",...))
-  }
-  invisible(x)  
-}
+### publish.anova.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds
+## created: Feb 24 2015 (14:51) 
+## Version: 
+## last-updated: Feb 24 2015 (14:51) 
+##           By: Thomas Alexander Gerds
+##     Update #: 1
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+
+
+
+#----------------------------------------------------------------------
+### publish.anova.R ends here
