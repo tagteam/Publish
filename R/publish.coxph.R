49275a62405c0a51cd4591ad8e5c42fe116c7cc7
Thomas Alexander Gerds <tag@biostat.ku.dk>
2015-05-26 17:47:03 +0200

    now coxph cannot delete missing values?

--- R/publish.coxph.R
+++ R/publish.coxph.R
@@ -17,23 +17,23 @@
 ##' library(survival)
 ##' data(pbc)
 ##' pbc$edema <- factor(pbc$edema,levels=c("0","0.5","1"),labels=c("0","0.5","1"))
-##' fit = coxph(Surv(time,status!=0)~age+sex+edema+log(bili)+log(albumin)+log(protime),
-##'             data=pbc)
+##' fit = coxph(Surv(time,status!=0)~age+sex+edema+log(bili)+log(albumin),
+##'             data=na.omit(pbc))
 ##' publish(fit)
 ##' ## forest plot
-##' plot(publish(fit))
+##' plot(publish(fit),cex=1.3)
 ##'
 ##' publish(fit,ci.digits=2,pvalue.eps=0.01,pvalue.digits=2,pvalue.stars=TRUE)
 ##' publish(fit,ci.digits=2,ci.handler="prettyNum",pvalue.eps=0.01,pvalue.digits=2,pvalue.stars=TRUE)
 ##' publish(fit,ci.digits=2,ci.handler="sprintf",pvalue.eps=0.01,pvalue.digits=2,pvalue.stars=TRUE,ci.trim=FALSE)
 ##' 
 ##' fit2 = coxph(Surv(time,status!=0)~age+sex+edema+log(bili,base=2)+log(albumin)+log(protime),
-##'     data=pbc)
+##'     data=na.omit(pbc))
 ##' publish(fit2)
 ##'
 ##' # with cluster variable
 ##' fit3 = coxph(Surv(time,status!=0)~age+cluster(sex)+edema+log(bili,base=2)+log(albumin)+log(protime),
-##'     data=pbc)
+##'     data=na.omit(pbc))
 ##' publish(fit3)
 ##'
 ##' # with strata and cluster variable
