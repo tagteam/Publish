860e7fe8799ea6376fddc8dc1bd964b9fd3bb0d7
Thomas Alexander Gerds <tag@biostat.ku.dk>
2017-10-22 13:03:45 +0200

    deleted unused stuff

--- R/ci.prop.R
+++
@@ -1,6 +0,0 @@
-ci.prop <- function(x,n,...){
-  out <- binom.test(x,n,...)
-  class(out) <- "ci.prop"
-  out
-}
-  
--- R/publish.ci.prop.R
+++
@@ -1,7 +0,0 @@
-##' @export
-publish.ci.prop <- function(object,digits=2,percent=TRUE,format="[l;u]",...){
- e <- if(percent) format(100*object$estimate,digits=digits,nsmall=digits) else format(object$estimate,digits=digits,nsmall=digits)
- l <- if(percent) format(100*object$conf.int[1],digits=digits,nsmall=digits) else format(object$conf.int[1],digits=digits,nsmall=digits)
- u <- if(percent) format(100*object$conf.int[2],digits=digits,nsmall=digits) else format(object$conf.int[2],digits=digits,nsmall=digits)
- paste(e,formatCI(l,u,format=format,...))
-}
