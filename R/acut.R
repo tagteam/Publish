99dc0b0063f8ac2155d30a39b1366c2c3279ebc0
Anders Munch <amnudn@gmail.com>
2018-08-21 14:32:36 +0200

    examples probably formatted

--- R/acut.R
+++ R/acut.R
@@ -1,6 +1,6 @@
 ##' A version of \code{cut} that easily formats the labels and places breaks by default.
 ##'
-##' The formats are supplied by specifiyng the text around the lower (%l) and upper (%l) value (see examples).
+##' The formats are supplied by specifiyng the text around the lower (\%l) and upper (\%l) value (see examples).
 ##' If user specified breaks are supplied, the default labels from \code{cut} are used.
 ##' If automatic breaks are used, the default labels are a slight modification at the end point of the default from \code{cut}
 ##' All this can of course be adjusted manually through the format functionality (see below). 
@@ -10,7 +10,7 @@
 ##' @title Automatic selection and formatting of breaks in \code{cut}
 ##' @param x a numeric vector which is to be converted to a factor by cutting (passed directly to \code{cut}).
 ##' @param n number of bins to create based on the empirical quantiles of x. This will be overruled if \code{breaks} is supplied.
-##' @param format string used to make labels. %l and %u identifies the lower and upper value of the breaks respectively. See examples.
+##' @param format string used to make labels. \%l and \%u identifies the lower and upper value of the breaks respectively. See examples.
 ##' @param format.low string used specifically on the lowest label.
 ##' @param format.high string used specifically on the highest label.
 ##' @param dig.lab integer which is used when labels are not given. It determines the number of digits used in formatting the break numbers. (Passed directly to \code{cut}.)
@@ -30,17 +30,30 @@
 ##' age.groups <- acut(Diabetes$age,format="%l-%u",breaks=seq(0,100,by=10))
 ##' table(age.groups)
 ##'
-##' age.groups <- acut(Diabetes$age,format="%l-%u", format.low="below %u", format.high="above %l", breaks=c(0, seq(20,80,by=10), Inf))
+##' age.groups <- acut(Diabetes$age,
+##'                    format="%l-%u",
+##'                    format.low="below %u",
+##'                    format.high="above %l",
+##'                    breaks=c(0, seq(20,80,by=10), Inf))
 ##' table(age.groups)
 ##' 
-##' BMI.groups <- acut(Diabetes$BMI,format="BMI between %l and %u",format.low="BMI below %u",format.high="BMI above %l")
+##' BMI.groups <- acut(Diabetes$BMI,
+##'                    format="BMI between %l and %u",
+##'                    format.low="BMI below %u",
+##'                    format.high="BMI above %l")
 ##' table(BMI.groups)
 ##' org(as.data.frame(table(BMI=BMI.groups)))
 ##'
-##' ## Instead of using the quantiles, we can specify equally spaced breaks, but still get the same formatting
-##' BMI.grouping <- seq(min(Diabetes$BMI,na.rm=TRUE), max(Diabetes$BMI,na.rm=TRUE), length.out=6)
+##' ## Instead of using the quantiles, we can specify equally spaced breaks,
+##' ## but still get the same formatting
+##' BMI.grouping <-
+##'    seq(min(Diabetes$BMI,na.rm=TRUE), max(Diabetes$BMI,na.rm=TRUE), length.out=6)
 ##' BMI.grouping[1] <- -Inf # To get all included
-##' BMI.groups <- acut(Diabetes$BMI,breaks=BMI.grouping,format="BMI between %l and %u",format.low="BMI below %u",format.high="BMI above %l")
+##' BMI.groups <- acut(Diabetes$BMI,
+##'                    breaks=BMI.grouping,
+##'                    format="BMI between %l and %u",
+##'                    format.low="BMI below %u",
+##'                    format.high="BMI above %l")
 ##' table(BMI.groups)
 ##' org(as.data.frame(table(BMI=BMI.groups)))
 ##' 
